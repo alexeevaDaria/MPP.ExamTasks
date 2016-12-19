585fa795d96208f55816214e074313a2fd2c8bf3
maximka777 <maksim.borovskij@yandex.ru>
2016-12-19 18:16:42 +0300

    Task09 Parallel

---
+++ Task09.Parallel/Task09.Parallel/Parallel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task09.Parallel
+{
+    class Parallel
+    {
+        public static void WaitAll(Func<bool>[] funcs)
+        {
+            List<Task<bool>> tasks = new List<Task<bool>>();
+            Task<bool> task;
+            foreach(Func<bool> func in funcs)
+            {
+                task = new Task<bool>(func);
+                tasks.Add(task);
+                task.Start();
+            }
+            Task.WaitAll(tasks.ToArray());
+        }
+    }
+}
---
+++ Task09.Parallel/Task09.Parallel/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task09.Parallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.WaitAll(new Func<bool>[] { Do, Do, Do, Do, Do, Do, Do, Do, Do, Do, Do, Do, Do, Do, Do });
+            Console.WriteLine("End");
+            Console.Read();
+        }
+
+        static bool Do()
+        {
+            Console.WriteLine("Do start");
+            Thread.Sleep(1000);
+            Console.WriteLine("Do end");
+            return true;
+        }
+    }
+}
