ea0b28093799cff2689856cd92e7389595a2918e
maximka777 <maksim.borovskij@yandex.ru>
2016-12-19 20:30:00 +0300

    Task11 Enumerator extension

---
+++ Task11.EnumeratorExtension/Task11.EnumeratorExtension/EnumeratorExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.EnumeratorExtension
+{
+    static class EnumeratorExtension
+    {
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            List<T> result = new List<T>();
+            foreach(T t in source)
+            {
+                if (predicate(t))
+                {
+                    result.Add(t);
+                }
+            }
+            return result.AsEnumerable();
+        }
+    }
+}
---
+++ Task11.EnumeratorExtension/Task11.EnumeratorExtension/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.EnumeratorExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 3, 4, 5, 6, 2, 7, 10, 50, 50, 38, 58 };
+            PrintList<int>(list);
+            IEnumerable<int> result = list.Where(x => x < 10);
+            PrintList<int>(result);
+            Console.Read();
+        }
+
+        static void PrintList<T>(IEnumerable<T> list)
+        {
+            foreach(T t in list)
+            {
+                Console.Write(t + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
