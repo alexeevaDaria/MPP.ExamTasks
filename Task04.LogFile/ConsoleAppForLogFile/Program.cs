fd6536c0a53fb695825e8557c08e4375d7bb8cb0
maximka777 <maksim.borovskij@yandex.ru>
2016-12-19 12:44:49 +0300

    Task04 LogFile class

---
+++ Task04.LogFile/ConsoleAppForLogFile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task04.LogFile;
+
+namespace ConsoleAppForLogFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogFile log = new LogFile("log.txt");
+            for(int i = 0; i < 10; i++)
+            {
+                log.Write(string.Format("Current number is {0}", i));
+            }
+            Console.WriteLine("Всё записано. Ентер чтобы закрыть логгер");
+            Console.Read();
+            log.Dispose();
+        }
+    }
+}
---
+++ Task04.LogFile/Task04.LogFile/LogFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task04.LogFile
+{
+    public class LogFile: IDisposable
+    {
+        private StreamWriter streamWriter;
+
+        public LogFile(string pathToFile)
+        {
+            streamWriter = new StreamWriter(pathToFile);
+        }
+
+        public void Dispose()
+        {
+            streamWriter.Close();
+        }
+
+        public void Write(string str)
+        {
+            String resultString = string.Format("{0} {1} {2}", DateTime.Now, Thread.CurrentThread.ManagedThreadId, str);
+            streamWriter.WriteLine(resultString);
+        }
+    }
+}
