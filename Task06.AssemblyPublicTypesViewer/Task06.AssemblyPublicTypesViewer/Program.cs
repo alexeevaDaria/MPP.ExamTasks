6d9f500869992d39dd8b35b351f86808eaedbe1f
maximka777 <maksim.borovskij@yandex.ru>
2016-12-19 17:53:18 +0300

    Task06 AssemblyPublicTypesViewer

---
+++ Task06.AssemblyPublicTypesViewer/Task06.AssemblyPublicTypesViewer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06.AssemblyPublicTypesViewer
+{
+    class TypeComparator : IComparer<TypeInfo>
+    {
+        public int Compare(TypeInfo x, TypeInfo y)
+        {
+            int result =  string.Compare(x.Namespace, y.Namespace);
+            if (result != 0)
+                return result;
+            result = string.Compare(x.Name, y.Name);
+            return result;
+        }
+    }
+
+    class Program
+    {
+
+        static bool FilterMethod(TypeInfo typeInfo)
+        {
+            return typeInfo.IsPublic;
+        }
+
+        static void Main(string[] args)
+        {
+            string pathToAssembly = args[0];
+            Console.WriteLine(pathToAssembly);
+            Assembly assembly = Assembly.LoadFrom(pathToAssembly);
+            List<TypeInfo> typeInfoList = assembly.DefinedTypes.Where(FilterMethod).ToList<TypeInfo>();
+            typeInfoList.Sort(new TypeComparator());
+            PrintTypeInfoList(typeInfoList);
+            Console.Read();
+        }
+
+        static void PrintTypeInfoList(List<TypeInfo> typeInfoList)
+        {
+            foreach(TypeInfo typeInfo in typeInfoList)
+            {
+                Console.WriteLine(typeInfo);
+            }
+        }
+    }
+}
