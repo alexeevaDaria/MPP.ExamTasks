db2a548bfb3f1356f84eb3e1f07bf20120261713
maximka777 <maksim.borovskij@yandex.ru>
2016-12-19 12:17:04 +0300

    Task02 TaskQueue

---
+++ Task02.TaskQueue/ConsoleAppForTaskQueue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Task02.TaskQueue;
+
+namespace ConsoleAppForTaskQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskQueue taskQueue = new TaskQueue(5);
+            for(int i = 0; i < 20; i++)
+            {
+                taskQueue.EnqueueTask(Taskmethod);
+            }
+            taskQueue.Wait();
+
+        }
+
+        static void Taskmethod()
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
---
+++ Task02.TaskQueue/Task02.TaskQueue/TaskQueue.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task02.TaskQueue
+{
+    public class TaskQueue : IDisposable
+    {
+        public delegate void TaskDelegate();
+        private ConcurrentQueue<TaskDelegate> tasks; 
+
+        private List<Thread> threads;
+        private bool work = true;
+
+
+        public TaskQueue(int threadCount)
+        {
+            threads = new List<Thread>();
+            tasks = new ConcurrentQueue<TaskDelegate>();
+            Thread thread;
+            for(int i = 0; i < threadCount; i++)
+            {
+                thread = new Thread(new ThreadStart(Run));
+                threads.Add(thread);
+            }
+            for(int i = 0; i < threadCount; i++)
+            {
+                threads[i].Start();
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate taskDelegate)
+        {
+            tasks.Enqueue(taskDelegate);
+        }
+
+
+        private void Run()
+        {
+            while (work)
+            {
+                try
+                {
+                    GetTask()();
+                }
+                catch(Exception exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+            }       
+        }
+
+        private TaskDelegate GetTask()
+        {
+            TaskDelegate taskDelegate;
+            while (!tasks.TryDequeue(out taskDelegate))
+                Thread.Sleep(100);
+            return taskDelegate;
+        }
+
+        public void Dispose()
+        {
+            foreach(Thread thread in threads)
+            {
+                thread.Interrupt();
+            }
+        }
+
+        public void Wait()
+        {
+            foreach(Thread thread in threads)
+            {
+                thread.Join();
+            }
+        }
+    }
+}
