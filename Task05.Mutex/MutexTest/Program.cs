2786f0b71ce02f6ec62687d96a6558f109c3422d
maximka777 <maksim.borovskij@yandex.ru>
2016-12-19 14:10:31 +0300

    Task05 Mutex but without recursion

---
+++ Task05.Mutex/MutexTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MutexTest
+{
+    class Program
+    {
+        private static Task05.Mutex.Mutex mutex;
+        private static int number = 0;
+
+        static void Main(string[] args)
+        {
+            mutex = new Task05.Mutex.Mutex();
+
+            Thread threadDec = new Thread(RunDec);
+            Thread threadInc = new Thread(RunInc);
+            threadDec.Start();
+            threadInc.Start();
+            threadDec.Join();
+            threadInc.Join();
+            Console.WriteLine(number);
+            Console.Read();
+        }
+
+        static void RunDec()
+        {
+            for(int i = 0; i < 100000000; i++)
+            {
+                mutex.Lock();
+                number -= 1;
+                mutex.Unlock();
+            }
+        }
+
+        static void RunInc()
+        {
+            for (int i = 0; i < 100000000; i++)
+            {
+                mutex.Lock();
+                number += 1;
+                mutex.Unlock();
+            }
+        }
+    }
+}
---
+++ Task05.Mutex/Task05.Mutex/Mutex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task05.Mutex
+{
+    public class Mutex
+    {
+        private int locked = 0;
+
+        public void Lock()
+        {
+            while (Interlocked.CompareExchange(ref locked, 1, 0) != 0)
+            {
+                Thread.Sleep(10);
+            }
+        }
+
+        public void Unlock()
+        {
+            Interlocked.Exchange(ref locked, 0);
+        }
+    }
+}
